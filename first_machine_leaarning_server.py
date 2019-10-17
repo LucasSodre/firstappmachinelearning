fff7518e5962a12e47ba13908185237fda51c924
LucasSodre <lucassodrebam8@gmail.com>
2019-10-16 21:57:57 -0300

    Criando servidor com cherrypy para rodar os algoritmos de predicao

---
+++ first_machine_leaarning_server.py
@@ -0,0 +1,49 @@
+
+import string
+import cherrypy
+import pandas as pd
+import numpy as np
+%matplotlib inline
+
+from sklearn.model_selection  import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+class RiscoCredito(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def probabilidade_risco_de_credito(self, historico_da_consulta=1,divida=1,renda=1000,garantia=1):
+        df1 = pd.read_csv("dados_limitecredito.csv")
+        data_treino = np.array(df1[['historico_credito', 'divida', 'garantias', 'renda']])
+        data_classif = np.array(df1['Risco'])
+        X_treino, X_teste, Y_treino, Y_teste = train_test_split(data_treino, data_classif, test_size=0.30)
+        modelo_tree = DecisionTreeRegressor()
+        arvore_classificacao = modelo_tree.fit(x_treino, y_treino)
+        #Executa a classificação
+        res = modelo_tree.predict([[int(renda_anual),int(historico_do_credito), int(divida), int(garantia)]])
+
+        #Encontra o valor da confidência
+        probabilidade = modelo_tree.predict_proba([[int(renda_anual), int(historico_do_credito), int(divida), int(garantia)]])
+
+        if res == 1:
+            classificacao = "Baixo"
+        elif res == 2:
+            classificacao = "Moderado"
+        else:
+            classificacao = "Alto"
+        
+        risco_credito = "Alto"
+        probabilidade = 0
+        dados = {
+            "renda":renda,
+            "risco_credito":classificacao,
+            "probabilidade":probabilidade,
+        }
+        return dadoss
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(RiscoCredito())
